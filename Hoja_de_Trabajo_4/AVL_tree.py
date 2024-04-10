12ff61ff66a2f2458c1cf3bff1f70b6057cd8d4c
govandoh <govandoh@miumg.edu.gt>
2024-04-10 16:05:41 -0600

    AVL

--- Hoja_de_Trabajo_4/AVL_tree.py
+++ Hoja_de_Trabajo_4/AVL_tree.py
@@ -1,86 +1,104 @@
-# Archivo .py para implementar toda la lógica de arbol AVL
-import csv
-import time
+# Codigo que implementa AVL 
 
-class nodoArbol:
-    def __init__(self, valor):
-        self.izq = None
-        self.der = None
-        self.padre  = None
-        self.valor = valor
-        
-class ABB:
+from graphviz import Digraph
+
+class Node:
+    def __init__ (self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
     def __init__(self):
-        self.raiz = None
+        self.root = None
 
-    def insert(self, valor):
-        self.raiz = self._insert(valor, self.raiz)
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
 
-    def _insert(self, valor, nodo):
-        if nodo is None:
-            return nodoArbol(valor)
-        
-        if(valor < nodo.valor):
-            nodo.izq = self._insert(valor, nodo.izq)
-        elif(valor > nodo.valor):
-            nodo.der = self._insert(valor, nodo.der)
-        
-        return nodo
-
-    def delete(self, valor):
-        self.raiz = self._delete(self.raiz, valor)
-
-    def _delete(self, node, valor):
+    def _insert(self, node, key):
         if node is None:
-            return node
-
-        if valor < node.valor:
-            node.izq = self._delete(node.izq, valor)
-        elif valor > node.valor:
-            node.der = self._delete(node.der, valor)
+            return Node(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
         else:
-            if node.izq is None:
-                return node.der
-            elif node.der is None:
-                return node.izq
-
-            node.valor = self._find_min(node.der).valor
-            node.der = self._delete(node.der, node.valor)
-
+            node.right = self._insert(node.right, key)
+        self._update_height(node)
+        balance_factor = self._get_balance_factor(node)
+        if balance_factor == 2:
+            if self._get_balance_factor(node.left) < 0:
+                node.left = self._rotate_left(node.left)
+            node = self._rotate_right(node)
+        elif balance_factor == -2:
+            if self._get_balance_factor(node.right) > 0:
+                node.right = self._rotate_right(node.right)
+            node = self._rotate_left(node)
         return node
 
-    def _find_min(self, node):
-        while node.izq is not None:
-            node = node.izq
-        return node
-    
-    def _find_max(self, node):
-        while node.der is not None:
-            node = node.der
-        return node
-    
-    def inorder (self,nodo):
-        if nodo is not None:
-            if nodo.izq != None:
-                self.inorder(nodo.izq)
-            if(nodo.der != None):
-                self.inorder(nodo.der)
-            print(nodo.valor)
-    
-    def cargarArchivo(self):
-        archivo_csv = 'Hoja_de_Trabajo_4\source\Air_Quality.csv'
-        with open(archivo_csv, newline='', encoding='utf-8') as csvfile:
-            lector_csv = csv.DictReader(csvfile)
-            for fila in lector_csv:
-                #id =  int(fila['Unique ID'])
-                #name = str(fila['Name'])
-                #place = str(fila['Geo Type Name'])
-                #date = str(fila['Start_Date'])
-                valor = fila['Unique ID']
-                self.insert(valor)
-                
-abb = ABB()
-abb.cargarArchivo()
-abb.inorder(abb.raiz)
+    def _rotate_left(self, rotation_node):
+        pivot_node = rotation_node.right
+        rotation_node.right = pivot_node.left
+        pivot_node.left = rotation_node
+        self._update_height(rotation_node)
+        self._update_height(pivot_node)
+        return pivot_node
 
-#abb.delete()
+    def _rotate_right(self, rotation_node):
+        pivot_node = rotation_node.left
+        rotation_node.left = pivot_node.right
+        pivot_node.right = rotation_node
+        self._update_height(rotation_node)
+        self._update_height(pivot_node)
+        return pivot_node
+
+    def _update_height(self, node):
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+
+    def _get_balance_factor(self, node):
+        if node is None:
+            return 0
+        return self._get_height(node.left) - self._get_height(node.right)
+
+    def _get_height(self, node):
+        if node is None:
+            return 0
+        return node.height
+    
+    def generar_dot(self,node, dot):
+        if node is not None:
+            dot.node(str(node.key))
+        if node.left is not None:
+            dot.edge(str(node.key), str(node.left.key))
+            self.generar_dot(node.left, dot)
+        if node.right is not None:
+            dot.edge(str(node.key), str(node.right.key))
+            self.generar_dot(node.right, dot)
+    
+    def view_Tree(self):
+        dot = Digraph()
+        self.generar_dot(self.root, dot)
+        dot.render('avl', format='png',view=True, cleanup=True)
+        
+    def print_tree(self):
+        def print_tree_node(node, indent=0):
+            if node is not None:
+                print_tree_node(node.right, indent + 4)
+                print("  " * indent + str(node.key))
+                print_tree_node(node.left, indent + 4)
+
+        return print_tree_node(self.root)
+
+# if __name__ == "__main__":
+#     avl_tree = AVLTree()
+#     for value in [1, 2, 3, 4, 5, 6, 7, 15, 14, 13, 12, 11, 10, 9, 8]:
+#         avl_tree.insert(value)
+#     avl_tree.view_Tree()
+#     avl_tree.print_tree()
+
+avl = AVLTree()
+
+for value in [1, 2, 3, 4, 5, 6, 7, 15, 14, 13, 12, 11, 10, 9, 8]:
+    avl.insert(value)
+    
+avl.print_tree()
+avl.view_Tree()

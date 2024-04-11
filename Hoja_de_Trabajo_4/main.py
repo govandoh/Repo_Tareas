5143d23dbbaa671f6728a38ba3b94677d0b5df8d
govandoh <govandoh@miumg.edu.gt>
2024-04-10 19:06:47 -0600

    prueba api

--- Hoja_de_Trabajo_4/main.py
+++ Hoja_de_Trabajo_4/main.py
@@ -1,1 +1,15 @@
 # Archivo main para hacer logica del API. 
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Ruta principal
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Hello, World!'})
+            
